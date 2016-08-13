ed9c415dbd36488b7b62ea19a0a5967ce3ab0ea1
Chris Powers <cjpowers3@gmail.com>
2016-08-13 16:22:32 -0700

    My job postings app! Initial commit. Purpose is to keep all applications in the same place.

---
+++ jobList/models.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class urlList(models.Model):
+    title = models.CharField(max_length = 200)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    posting_url = models.URLField(max_length = 200, default = '')
+    posting_priority = models.BooleanField(default = False)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
