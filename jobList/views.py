3b5d550daf7c6c8566cebd536758ead97ce750e5
Chris Powers <cjpowers3@gmail.com>
2016-08-16 18:56:38 -0700

    added user functionality

--- jobList/views.py
+++ jobList/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import UrlList
 from .forms import PostForm
@@ -8,17 +9,13 @@ def jobs_list(request):
     posts = UrlList.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'jobList/jobs_list.html', {'posts': posts})
 
-'''def post_new(request):
-    form = PostForm()
-    return render(request, 'jobList/post_edit.html', {'form': form})
-'''
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            #post.author = request.user
+            post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('jobs_list')
@@ -26,12 +23,28 @@ def post_new(request):
         form = PostForm()
     return render(request, 'jobList/post_edit.html', {'form': form})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(UrlList, pk=pk)
     return render(request, 'jobList/post_detail.html', {'post': post})
 
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(UrlList, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'jobList/post_edit.html', {'form': form})
 
+@login_required
 def post_remove(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(UrlList, pk=pk)
     post.delete()
-    return redirect('jobList.views:jobs_list')
+    return redirect('jobs_list')
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.contrib.auth import views
+
 from django.contrib import admin
+import django.contrib.auth.views
+admin.autodiscover()
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('jobList.urls')),
 ]
